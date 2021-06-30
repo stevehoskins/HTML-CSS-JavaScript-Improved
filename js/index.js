4cc7096bfb6b5fa6ed9fb29bad8338da8d5d6b1a
Steve Hoskins <80352473+stevehoskins@users.noreply.github.com>
2021-06-30 22:57:32 +0100

    Big partial update

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+
+function ingredientsHover() {
+    document.getElementById("ingredients").style.fontSize = "300%";
+}
+function ingredientsNormal() {
+    document.getElementById("ingredients").style.fontSize = "100%";
+}  	
+function sectionsHover() {
+    document.getElementById("sections-for-reference").style.fontSize = "300%";
+}
+function sectionsNormal() {
+    document.getElementById("sections-for-reference").style.fontSize = "100%";
+}  
